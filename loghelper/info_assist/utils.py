275c60c22c2795f6843e1a0dc73040ef69f0dd16
Siarhei Khomich <limixserega@gmail.com>
2024-07-01 17:12:40 +0300

    refact -datamixin

--- loghelper/info_assist/utils.py
+++ loghelper/info_assist/utils.py
@@ -12,21 +12,3 @@ MAPPING = {
         'X': 'Х',
         'Y': 'У',
     }
-
-
-class DataMixin:
-    paginate_by = 5
-
-    def get_user_context(self, **kwargs):
-        context = kwargs
-        cats = Category.objects.annotate(Count('posts'))
-
-        user_menu = menu.copy()
-        if not self.request.user.is_authenticated:
-            user_menu.pop(1)
-
-        context['menu'] = user_menu
-        context['cats'] = cats
-        if 'cat_selected' not in context:
-            context['cat_selected'] = 0
-        return context
