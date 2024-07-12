e550ff2922f5a6d8eeddbb75b45b2d5cd8e1a6ec
Siarhei Khomich <limixserega@gmail.com>
2024-07-12 15:39:45 +0300

    static root added

--- loghelper/loghelper/settings/_base.py
+++ loghelper/loghelper/settings/_base.py
@@ -111,6 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 # STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
