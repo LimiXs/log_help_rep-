5fc0e6c3d0680f449be02ed7daa73302b0b28707
Siarhei Khomich <limixserega@gmail.com>
2024-08-20 17:18:04 +0300

    update auth-home module + refact

--- loghelper/info_assist/forms.py
+++ loghelper/info_assist/forms.py
@@ -1,13 +1,6 @@
 # forms.py
 from django import forms
-
 from info_assist.models import PDFDataBase
-
-
-class ExtraButtonsForm(forms.Form):
-    button1 = forms.CharField(label='Кнопка 1', max_length=100, widget=forms.HiddenInput())
-    button2 = forms.CharField(label='Кнопка 2', max_length=100, widget=forms.HiddenInput())
-    button3 = forms.CharField(label='Кнопка 3', max_length=100, widget=forms.HiddenInput())
 
 
 class PDFDataBaseAdminForm(forms.ModelForm):
